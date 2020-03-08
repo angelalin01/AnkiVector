4960fb648c828f13e44ab0699af080827d33e9b4
Angela Lin <30933507+angelalin01@users.noreply.github.com>
2020-03-07 22:50:12 -0500

    Update detectRedColor.py

--- detectRedColor.py
+++ detectRedColor.py
@@ -2,14 +2,36 @@ import numpy as np
 import argparse
 import cv2
 
+import anki_vector
+import time
+from PIL import Image
+
+
+
+with anki_vector.Robot(enable_camera_feed=True) as robot:
+    for _ in range(30):
+        while not robot.camera.latest_image:
+            time.sleep(1.0)
+        image = robot.camera.latest_image
+        #image = image.resize((184,96))
+	robot.camera.latest_image.save("temp.png", 'PNG') #check syntax
+    
+        #screen_data = anki_vector.screen.convert_image_to_screen_data(image)
+        
+        #robot.screen.set_screen_with_image_data(screen_data, 0.5)
+        time.sleep(0.5)
+
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", help = "path to the image")
+#args = vars(ap.parse_args())
+
 # load the image
-image = cv2.imread(args["image"])
+# change based on what path
+image = cv2.imread('temp.png')
 
 # define the list of boundaries
+# boudnaries for red 
 boundaries = [
 	([17, 15, 100], [50, 56, 200])
 ]
